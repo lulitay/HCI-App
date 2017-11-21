51c4d84f824b62edda11fb834668b02c280f9b2a
arodriguez1415 <arodriguez@itba.edu.ar>
2017-11-21 13:55:53 -0300

    Add files via upload

---
+++ app/src/main/java/com/example/myfirstapp/ResultStateBoolean.java
@@ -0,0 +1,21 @@
+package com.example.myfirstapp;
+
+/**
+ * Created by Ariel on 20/11/2017.
+ */
+
+public class ResultStateBoolean {
+    boolean result;
+
+    public ResultStateBoolean(boolean result){
+        this.result = result;
+    }
+
+    public boolean getResult(){
+        return result;
+    }
+
+    public void setResult(boolean result){
+        this.result = result;
+    }
+}
---
+++ app/src/main/java/com/example/myfirstapp/ResultString.java
@@ -0,0 +1,21 @@
+package com.example.myfirstapp;
+
+/**
+ * Created by Ariel on 20/11/2017.
+ */
+
+public class ResultString {
+    String result;
+
+    public ResultString(String result){
+        this.result = result;
+    }
+
+    public String getResult(){
+        return result;
+    }
+
+    public void setResult(String result){
+        this.result = result;
+    }
+}
