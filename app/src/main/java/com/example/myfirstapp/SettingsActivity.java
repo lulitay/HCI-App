8ecc6f3bf4a49f0b122b2dd6cd2521fdbdabaef1
arodriguez1415 <arodriguez@itba.edu.ar>
2017-11-23 14:04:53 -0300

    Update SettingsActivity.java

--- app/src/main/java/com/example/myfirstapp/SettingsActivity.java
+++ app/src/main/java/com/example/myfirstapp/SettingsActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
@@ -32,6 +33,17 @@ public class SettingsActivity extends AppCompatActivity {
 
         mFancyAdapter = new FancyAdapter(data);
         listView.setAdapter(mFancyAdapter);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // Respond to the action bar's Up/Home button
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     private class FancyAdapter extends BaseAdapter {
